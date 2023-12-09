29dc8b5876b74b02436d9ad918df62012fb67c6a
fzdwx <likelovec@gmail.com>
2023-12-09 14:41:25 +0800

    fix: select

--- src/index.tsx
+++ src/index.tsx
@@ -417,7 +417,7 @@ const Command = React.forwardRef<HTMLDivElement, CommandProps>((props, forwarded
 
     function selectFirstItem2() {
         store.setState('value', '')
-        schedule(1, selectFirstItem)
+        schedule(7, selectFirstItem)
     }
 
     /** Filters the current items. */
